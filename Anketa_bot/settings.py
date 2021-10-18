8fb6d31174fd022f13e6465cce94fa422e2853fb
KING PC <king97queen99@gmail.com>
2021-10-18 22:03:59 +0500

    setting

--- Anketa_bot/settings.py
+++ Anketa_bot/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Anketa_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
