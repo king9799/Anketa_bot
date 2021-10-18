ea29962fa9dc7522795d8fafdf49f9c96f743757
KING PC <king97queen99@gmail.com>
2021-10-18 21:26:35 +0500

    setting

--- Anketa_bot/settings.py
+++ Anketa_bot/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,10 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static")
-STATICFILES_DIRS = ( os.path.join(BASE_DIR,"static")),
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), 'static']
 MEDIA_ROOT = os.path.join(BASE_DIR, "images")
